fb0891070def040f077689b3c0af86fb00d5814b
Yazed Jamal <muyaszed@gmail.com>
2018-01-13 23:42:23 +0000

    added room view

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,9 +1,64 @@
 import React, { Component } from 'react';
+import { Button, Image, List, Container, Segment } from 'semantic-ui-react'
 
 class Rooms extends Component {
 	render() {
 		return (
-			<h1>This is the room list</h1>
+		  <Segment.Group>
+		    <Segment>
+		    	<Button fluid>Create new room</Button>
+		    </Segment>
+		    <Segment>
+				<Container>
+
+
+					<List divided verticalAlign='middle'>
+					    <List.Item>
+					      <List.Content floated='right'>
+					        <Button>Join</Button>
+					      </List.Content>
+					      <List.Icon name='comments outline' size='large' verticalAlign='middle' />
+					      <List.Content>
+					         <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
+		        			 <List.Description as='a'>Updated 10 mins ago</List.Description>
+					      </List.Content>
+					    </List.Item>
+					    <List.Item>
+					      <List.Content floated='right'>
+					        <Button>Join</Button>
+					      </List.Content>
+					      <List.Icon name='comments outline' size='large' verticalAlign='middle' />
+					      <List.Content>
+					         <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
+		        			 <List.Description as='a'>Updated 10 mins ago</List.Description>
+					      </List.Content>
+					    </List.Item>
+					    <List.Item>
+					      <List.Content floated='right'>
+					        <Button>Join</Button>
+					      </List.Content>
+					      <List.Icon name='comments outline' size='large' verticalAlign='middle' />
+					      <List.Content>
+					         <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
+		        			 <List.Description as='a'>Updated 10 mins ago</List.Description>
+					      </List.Content>
+					    </List.Item>
+					    <List.Item>
+					      <List.Content floated='right'>
+					        <Button>Join</Button>
+					      </List.Content>
+					      <List.Icon name='comments outline' size='large' verticalAlign='middle' />
+					      <List.Content>
+					         <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
+		        			 <List.Description as='a'>Updated 10 mins ago</List.Description>
+					      </List.Content>
+					    </List.Item>
+					</List>
+
+				</Container>
+		    </Segment>
+		  </Segment.Group>
+		
 		);
 	}
 }
